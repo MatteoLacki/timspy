746c682141fb8839e97a49f310d87251c913d26c
matteo <matteo.lacki@gmail.com>
2020-06-22 17:50:08 +0200

    haring

---
+++ examples/writing_hdf5.py
@@ -0,0 +1,38 @@
+%load_ext autoreload
+%autoreload 2
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+pd.set_option('display.max_rows', 20)
+pd.set_option('display.max_columns', 500)
+from pathlib import Path
+import vaex as vx
+
+from timspy.timspy import TimsDIA
+from timspy.iterators import ranges
+from time import time
+
+p = Path('/home/matteo/Projects/bruker/BrukerMIDIA/MIDIA_CE10_precursor/20190912_HeLa_Bruker_TEN_MIDIA_200ng_CE10_100ms_Slot1-9_1_488.d')
+D = TimsDIA(p)
+output_folder = Path("/home/matteo/Projects/bruker/data_dumps/prec_prec_100ms")
+# D.to_hdf5(output_folder)
+
+df = vx.open_many([str(p) for p in output_folder.glob("*.hdf5")])
+df.plot(df.tof, df.scan, what=vx.stat.sum(df.i))
+plt.tight_layout()
+plt.show()
+
+# check if all stats are there
+x = df.count(df.i, binby=[df.scan], shape=1000)
+
+df.plot1d(df.scan, what=vx.agg.nunique(df.scan))
+df.
+
+?vx.agg.nunique
+df.agg
+
+
+S = df.groupby(df.scan).agg({'i':'sum'})
+np.sort(S.scan.values)
+
+S = df.groupby(df.scan).agg({'i':'sum'})
