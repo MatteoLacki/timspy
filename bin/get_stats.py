2414e7798fefc521891a3be7318cb2081ad8428c
MatteoLacki <matteo.lacki@gmail.com>
2021-03-10 13:17:45 +0100

    testing again

--- bin/get_stats.py
+++ bin/get_stats.py
@@ -1,9 +1,11 @@
 import argparse
+from datetime import datetime
 import numpy as np
 import pandas as pd
 import pathlib
 
 from timspy.df import TimsPyDF
+# from timspy.plot import heatmap
 
 ap = argparse.ArgumentParser(description='Calculate the Total Ion Current or peak counts for a given selection of points.',
   formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -14,10 +16,18 @@ ARG('folders',
     help="Path(s) to a timsTOF .d folder(s) containing 'analysis.tdf' and 'analysis.tdf_raw'.",
     type=pathlib.Path)
 
-ARG('--output',
-    help="Path where to save output to.",
-    default="",
+ARG('--output_folder',
+    help="Path to output folder to save output to.",
+    default=".",
     type=pathlib.Path)
+
+ARG('--save_plots',
+    help="Save plots of the folders.",
+    action='store_true')
+
+ARG('--save_computed_data',
+    help="Save raw data to the folder.",
+    action='store_true')
 
 ARG('--condition', 
     default="inv_ion_mobility < .0009*mz + .4744",
@@ -46,45 +56,36 @@ args = ap.parse_args()
 
 statistic_totals = []
 multiply_charged_statistic_totals = []
-# frames_stats_list = []
-# borders_list = []
 
 variables = ("mz","inv_ion_mobility") if args.statistic == "peak_count" else ("mz","inv_ion_mobility","intensity")
 
-# for folder in args.folders:
-#     if args.verbose:
-#         print(f"Dealing with: {folder}")
-#     D = TimsPyDF(folder)
-#     frames_stats, borders = D.bin_frames(variables=variables,
-#                                          min_row=50,
-#                                          max_row=2000,
-#                                          min_column=.3,
-#                                          max_column=1.8,
-#                                          bins_row=args.mz_bins_cnt,
-#                                          bins_column=args.inv_ion_mobility_bins_cnt,
-#                                          desaggregate=False,
-#                                          return_df=True, 
-#                                          verbose=args.verbose)
-#     # frames_stats_list.append(frames_stats)
-#     # borders_list.append(borders)
+analysis_time = datetime.now().strftime('%d_%m_%Y__%H_%M_%S')
 
-#     stats_long = pd.melt(frames_stats,
-#                          ignore_index=False,
-#                          value_name=args.statistic).reset_index()
-#     statistic_total = stats_long[args.statistic].sum()
-#     multiply_charged_statistic_total = stats_long.query(args.condition)[args.statistic].sum()
-#     statistic_totals.append(statistic_total)
-#     multiply_charged_statistic_totals.append(multiply_charged_statistic_total)
+for folder in args.folders:
+    if args.verbose:
+        print(f"Dealing with: {folder}")
+    D = TimsPyDF(folder)
+    frames_stats, borders = D.bin_frames(variables=variables,
+                                         bins_row=args.mz_bins_cnt,
+                                         bins_column=args.inv_ion_mobility_bins_cnt,
+                                         desaggregate=False,
+                                         return_df=True, 
+                                         verbose=args.verbose)
+    if args.save_computed_data:
+        frames_stats.to_csv(path_or_buf=args.output_folder/f"{folder.stem}_{args.statistic}_{analysis_time}_all.csv")
+    
+    stats_long = pd.melt(frames_stats,
+                         ignore_index=False,
+                         value_name=args.statistic).reset_index()
+    statistic_total = stats_long[args.statistic].sum()
+    multiply_charged_statistic_total = stats_long.query(args.condition)[args.statistic].sum()
+    statistic_totals.append(statistic_total)
+    multiply_charged_statistic_totals.append(multiply_charged_statistic_total)
 
-# out = pd.DataFrame({ "folder": [f.name for f in args.folders],
-#                      "path":   args.folders,
-#                      args.statistic: statistic_totals,
-#                      f"{args.statistic}_multiply_charged": multiply_charged_statistic_totals})
+summary_df = pd.DataFrame({ "folder": [f.name for f in args.folders],
+                     "path":   args.folders,
+                     args.statistic: statistic_totals,
+                     f"{args.statistic}_multiply_charged": multiply_charged_statistic_totals})
 
-if str(args.output) == ".":
-    output = f"./{args.statistic}.csv"
-else:
-    output = args.output
-
-print(output)
-# out.to_csv(path_or_buf=args.output, index=False)
+output_file = args.output_folder/f"{args.statistic}_{analysis_time}.csv"
+summary_df.to_csv(path_or_buf=output_file, index=False)
